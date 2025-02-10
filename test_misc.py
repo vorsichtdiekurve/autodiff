e29d4abf2fe7bd1b182ee649b7782d55cb8034fd
Mateusz Polański <mp.mateusz.polanski@gmail.com>
2025-02-10 22:10:16 +0100

    Add test cases for polynomials

--- test_misc.py
+++ test_misc.py
@@ -6,3 +6,13 @@ from x import X
 def test_x_with_argument_raises_invalid_argument_error():
     with pytest.raises(InvalidArgumentError):
         X(1)
+
+@pytest.mark.parametrize(
+    "x, x_0, res",
+    [
+        (X()**5 - 4 * X()**3 + 3 * X(), 2, 5 * 16 - 12 * 4 + 3),
+        (X()**2 - 3 * X()**(-3), 3, 2 * 3 + 9 / 81)
+    ]
+)
+def test_polynomials(x, x_0, res):
+    assert x.df(x_0) == pytest.approx(res)
---
+++ test_misc.py~
@@ -0,0 +1,8 @@
+import pytest
+
+from invalid_argument_error import InvalidArgumentError
+
+from x import X
+def test_x_with_argument_raises_invalid_argument_error():
+    with pytest.raises(InvalidArgumentError):
+        X(1)
