5953d92c8aa1a2f71bab2092a65e0c485caea431
Mateusz Polański <mp.mateusz.polanski@gmail.com>
2025-02-10 18:26:39 +0100

    Add test cases for division and power

--- test_arithmetic.py
+++ test_arithmetic.py
@@ -5,7 +5,6 @@ import pytest
 from x import X
 from sin import Sin
 from cos import Cos
-from base import Base
 
 @pytest.mark.parametrize(
     "x, y, x_0, res",
@@ -47,3 +46,39 @@ def test_subtraction(x, y, x_0, res):
 )
 def test_multiplication(x, y, x_0, res):
     assert (x * y).df(x_0) == pytest.approx(res)
+
+@pytest.mark.parametrize(
+    "x, y, x_0, res",
+    [
+        (Cos(X()), 5, math.pi * 0.5, -0.2),
+        (X(), Sin(X()), math.pi / 3, 2 * (1 - math.pi * math.sqrt(3))),
+        (2, Cos(X()), math.pi / 4 , 2 * math.sqrt(2)),
+    ]
+)
+def test_division(x, y, x_0, res):
+    assert (x / y).df(x_0) == pytest.approx(res)
+
+@pytest.mark.parametrize(
+    "x, y, x_0",
+    [
+        (X(), 0, 0),
+        (Cos(X()), Sin(X()), 0),
+        (Sin(X()), Cos(X()), math.pi * 0.5),
+    ]
+)
+def test_division_by_zero_raises_zero_division_error(x, y, x_0):
+    with pytest.raises(ZeroDivisionError):
+        (x / y).df(x_0)
+
+@pytest.mark.parametrize(
+    "x, y, x_0, res",
+    [
+        (X(), 2, 3, 6),
+        (X(), 9, 2, 9 * 256),
+        (X(), -2, 3, -13.5),
+        (X(), math.e, math.pi, math.e * math.pi ** (math.e - 1)),
+        (Cos(X()), 2, math.pi / 4, -1)
+    ]
+)
+def test_power(x, y, x_0, res):
+    assert (x ** y).df(x_0) == pytest.approx(res)
